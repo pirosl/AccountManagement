b850315ef41a0fc5d381fbca68f98e25509e4057
Lucian Piros <pirosl@yahoo.com>
2019-08-30 22:10:44 +0100

    Minor: add javadoc comments

--- app/src/main/java/com/lucianpiros/accountmanagement/restapi/UserAccountAPI.java
+++ app/src/main/java/com/lucianpiros/accountmanagement/restapi/UserAccountAPI.java
@@ -7,6 +7,9 @@ import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.POST;
 
+/**
+ * REST Restrofit API
+ */
 public interface UserAccountAPI {
     @POST("/auth/signup ")
     Call<SignupResponse> createUser(@Body SerializableSignup serializableSignup);
