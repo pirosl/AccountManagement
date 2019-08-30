fb06516e4adc16d8cbb4e8f467b69d27bb84e27d
Lucian Piros <pirosl@yahoo.com>
2019-08-30 17:10:02 +0100

    Feat: Me class implementation

---
+++ app/src/main/java/com/lucianpiros/accountmanagement/aidl/Me.java
@@ -0,0 +1,48 @@
+package com.lucianpiros.accountmanagement.aidl;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * Me class container. Implements Parcelable - holds all user account information
+ */
+public final class Me implements Parcelable {
+    private String name;
+    private String location;
+    private String birthdate;
+
+
+    protected Me(Parcel in) {
+        readFromParcel(in);
+    }
+
+    public static final Creator<Me> CREATOR = new Creator<Me>() {
+        @Override
+        public Me createFromParcel(Parcel in) {
+            return new Me(in);
+        }
+
+        @Override
+        public Me[] newArray(int size) {
+            return new Me[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+    
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeString(location);
+        dest.writeString(birthdate);
+    }
+
+    private void readFromParcel(Parcel parcel) {
+        name = parcel.readString();
+        location = parcel.readString();
+        birthdate = parcel.readString();
+    }
+}
