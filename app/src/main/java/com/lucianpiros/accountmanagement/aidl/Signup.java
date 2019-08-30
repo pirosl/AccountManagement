349dd739be329e592bd116872e4812b867eac8ed
Lucian Piros <pirosl@yahoo.com>
2019-08-30 17:05:10 +0100

    Feat: Signup class implementation

---
+++ app/src/main/java/com/lucianpiros/accountmanagement/aidl/Signup.java
@@ -0,0 +1,52 @@
+package com.lucianpiros.accountmanagement.aidl;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * Signup class container. Implements Parcelable - holds all information required to signup through REST service
+ */
+public final class Signup implements Parcelable {
+    private String name;
+    private String email;
+    private String password;
+    private String password2;
+
+
+    protected Signup(Parcel in) {
+        readFromParcel(in);
+    }
+
+    public static final Creator<Signup> CREATOR = new Creator<Signup>() {
+        @Override
+        public Signup createFromParcel(Parcel in) {
+            return new Signup(in);
+        }
+
+        @Override
+        public Signup[] newArray(int size) {
+            return new Signup[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        // default implementation
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(name);
+        parcel.writeString(email);
+        parcel.writeString(password);
+        parcel.writeString(password2);
+    }
+
+    private void readFromParcel(Parcel parcel) {
+        name = parcel.readString();
+        email = parcel.readString();
+        password = parcel.readString();
+        password2 = parcel.readString();
+    }
+}
