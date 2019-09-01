d223c503fddb300da2b03fe104bc9146593d12e0
Lucian Piros <pirosl@yahoo.com>
2019-09-01 14:44:22 +0100

    Fix: fix SignupResponse pojo

--- app/src/main/java/com/lucianpiros/accountmanagement/restapi/pojo/SignupResponse.java
+++ app/src/main/java/com/lucianpiros/accountmanagement/restapi/pojo/SignupResponse.java
@@ -6,18 +6,24 @@ import com.google.gson.annotations.SerializedName;
  * Signup request HTTP POST response
  */
 public class SignupResponse {
+
+    public class Data {
+        @SerializedName("user_uuid")
+        public String user_uuid;
+
+        @SerializedName("user_token")
+        public String user_token;
+
+        @SerializedName("api_token")
+        public String api_token;
+    }
+
     @SerializedName("message")
     public String message;
 
     @SerializedName("error_short_code")
     public String error_short_code;
 
-    @SerializedName("user_uuid")
-    public String user_uuid;
-
-    @SerializedName("user_token")
-    public String user_token;
-
-    @SerializedName("api_token")
-    public String api_token;
+    @SerializedName("data")
+    public Data data;
 }
