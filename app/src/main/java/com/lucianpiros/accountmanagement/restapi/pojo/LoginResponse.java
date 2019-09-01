906342d85e7dbe4c9dc48dedac289f00700ba343
Lucian Piros <pirosl@yahoo.com>
2019-09-01 14:44:41 +0100

    Fix: fix LoginResponse pojo

--- app/src/main/java/com/lucianpiros/accountmanagement/restapi/pojo/LoginResponse.java
+++ app/src/main/java/com/lucianpiros/accountmanagement/restapi/pojo/LoginResponse.java
@@ -6,15 +6,22 @@ import com.google.gson.annotations.SerializedName;
  * Login request HTTP POST response
  */
 public class LoginResponse {
+
+    public class Data {
+        @SerializedName("user_token")
+        public String user_token;
+
+        @SerializedName("api_token")
+        public String api_token;
+    }
+
     @SerializedName("message")
     public String message;
 
     @SerializedName("error_short_code")
     public String error_short_code;
 
-    @SerializedName("user_token")
-    public String user_token;
+    @SerializedName("data")
+    public Data data;
 
-    @SerializedName("api_token")
-    public String api_token;
 }
